178614c50390c99ab5e9699cab1e2880ca2fec7b
Balaji Sethuraman <Balaji Sethuraman>
2016-12-26 20:02:09 +0530

    First

---
+++ app/src/main/java/com/bseth/testing_sample/MainActivity.java
@@ -0,0 +1,19 @@
+package com.bseth.testing_sample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickGo(View view){
+        Intent i = new Intent(getApplicationContext(),SecondActivity.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/bseth/testing_sample/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.bseth.testing_sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bseth on 26-12-2016.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+}
